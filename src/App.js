39265bbd45e8b5acaa6b9f0f40b2f8db90917af4
Gokulprakash2005 <gokulprakash1301@gmail.com>
2024-09-24 22:59:50 +0530

    Adding About Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Indexes from './comp/indexpage/hello';
-
+import About from './comp/about/about';
 function App() {
   return (
     <>
     <Indexes/>
+    <About/>
     </>
   );
 }
